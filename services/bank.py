d38ef74012f626a883fec8787857ade805d4e392
AL-ANWARTECH <anwarsagirmustapha1@gmail.com>
2026-09-22 11:12:58 +0100

    feat: add customer management

--- services/bank.py
+++ services/bank.py
@@ -1,9 +1,11 @@
 from models.account import BankAccount
+from models.customer import Customer
 
 
 class Bank:
     def __init__(self):
         self.accounts = []
+        self.customers = []
 
     def add_account(self, account):
         self.accounts.append(account)
@@ -20,8 +22,20 @@ class Bank:
             if account.account_number == account_number:
                 self.accounts.remove(account)
                 return
-            
 
+    def display_accounts(self):
+        for account in self.accounts:
+            account.display_account()
+
+
+    def add_customer(self, customer):
+        self.customers.append(customer)
+
+    def find_customer(self, customer_id):
+        for customer in self.customers:
+            if customer.customer_id == customer_id:
+                return customer
+        return None
     
 
 bank = Bank()
@@ -36,3 +50,13 @@ account = bank.find_account(1002)
 print(account)
 bank.remove_account(1003)
 print(bank.accounts)
+
+bank.display_accounts()
+print(bank.customers)
+
+customer1 = Customer(1, "Anwar", "anwarsagirmustapha1@gmail.com", "09067508735")
+bank.customers
+bank.add_customer(customer1)
+print(bank.customers)
+customer = bank.find_customer(1)
+print(customer)
